611b478a5a97b13ccc3d4fea2e44f0fc91035f19
MauSolorzano <127962979+MauSolorzano@users.noreply.github.com>
2023-06-05 21:31:40 -0600

    Arreglo problema con la varible char
    No permitía la linea de restringir tamaño

--- GestionDeRiesgos/Models/Riesgos.cs
+++ GestionDeRiesgos/Models/Riesgos.cs
@@ -9,10 +9,12 @@ namespace GestionDeRiesgos.Models
         [Display(Name = "Código de riesgo")]
         [StringLength(100, ErrorMessage = "El código ingresado es demasiado largo")]
         public string codigoRiesgo { get; set; }
+
         [Required(ErrorMessage = "Es necesario ingresar el nombre del riesgo")]
         [Display(Name = "Nombre")]
         [StringLength(100, ErrorMessage = "El nombre ingresado es demasiado largo")]
         public string nombre { get; set; }
+
         [Required(ErrorMessage = "Es necesario ingresar la descripcion del riesgo")]
         [Display(Name = "Descripción")]
         [StringLength(500, ErrorMessage = "La descripcion que se ingreso es demasiado larga")]
@@ -22,20 +24,25 @@ namespace GestionDeRiesgos.Models
         [Display(Name = "Probabilidad")]
         [StringLength(100, ErrorMessage = "La probabilida que ingreso es demasiado larga")]
         public string probabilidad { get; set; }
+
+
         [Required(ErrorMessage = "Es necesario ingresar el impacto del riesgo")]
-        [Display(Name = "Nombre")]
+        [Display(Name = "Impacto")]
         public int impacto { get; set; }
+
         [Required(ErrorMessage = "Es necesario ingresar la categoría del riesgo")]
         [Display(Name = "Categoría")]
         [StringLength(100, ErrorMessage = "La categoría que se ingreso es demasiado larga")]
         public string categoria { get; set; }
+
         [Required(ErrorMessage = "Es necesario ingresar la fecha cuando se crea el riesgo")]
         [Display(Name = "Fecha de creación ")]
         public DateTime fecha { get; set; }
+
         [Required(ErrorMessage = "Es necesario ingresar el estado del riesgo")]
         [Display(Name = "Estado")]
-        [StringLength(1, ErrorMessage = "El nombre ingresado es demasiado largo")]
         public char estado { get; set; }
+
         [Required(ErrorMessage = "Es necesario ingresar las observaciones correspondientes del riesgo")]
         [Display(Name = "Observaciones")]
         [StringLength(500, ErrorMessage = "El nombre ingresado es demasiado largo")]
